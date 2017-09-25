a5de12514b9956f7201c65c443223a859e6b550e
Arthur Stemmer <apstemmer@hotmail.com>
2017-09-25 10:44:53 +0100

    some more config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry:{
+    dist: './index.js'
+  },
+  output:{
+    path: path.join(__dirname, 'dist'),
+    publicPath: 'dist/',
+    filename: 'dist.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: '../index.html',
+      chunks: ['dist']
+    })
+  ]
+}
