70c83f42b68ce55b0464abb2b19aecaf4f308089
Arthur Stemmer <apstemmer@hotmail.com>
2017-09-26 12:57:08 +0100

    needs better styling

--- src/AddressCard.js
+++ src/AddressCard.js
@@ -1,34 +1,21 @@
 import React, { Component } from 'react';
-import { bonds, InlineAccount, AccountLabel, AddressBond, AccountIcon } from 'parity-reactive-ui';
+import { bonds, InlineAccount, AccountLabel, InlineBalance, AccountIcon } from 'parity-reactive-ui';
 import { TimeBond } from 'oo7';
 import { formatBalance } from 'oo7-parity';
-import { Rspan } from 'oo7-react';
+import { ReactiveComponent, Rspan } from 'oo7-react';
 
 export default class AddressCard extends Component{
   constructor(){
     super();
   }
 
-  componentWillMount(){
-  }
-
   render(){
-    // bonds.allAccountsInfo.tie((list) => {
-    //   Object.keys.filter
-    // })
-    console.log('accinfo',bonds.allAccountsInfo);
+    console.log('accinfo');
     return (<div className="AddressCard">
-      <AccountIcon address={bonds.me} />
-      <Rspan>{bonds.balance(bonds.me).map(formatBalance)}</Rspan>
-      <Rspan>
-		      {bonds.allAccountsInfo((info)=>{
-            Object.keys(info).filter((key) => {
-              return (typeof accountList[key].uuid == 'undefined') && (!accountList[key].meta.contract) && !accountList[key].meta.wallet;
-            }).map((elem)=>{
-              return elem;
-            })
-          })}
-	   </Rspan>
+      <AccountIcon address={this.props.address} />
+      <InlineBalance value={bonds.balance(this.props.address)} />
+      <Rspan>{bonds.accountsInfo[this.props.address].name}</Rspan>
+      <span>{this.props.address}</span>
     </div>);
   }
 }
--- src/Addresses.js
+++ src/Addresses.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
+import { bonds } from 'parity-reactive-ui';
+import { Rspan } from 'oo7-react';
 
 import AddressCard from './AddressCard';
 
 import styles from './Addresses.css';
 
 export default class Addresses extends Component {
-
+  constructor(){
+    super();
+    this.state = {
+      accounts: []
+    }
+  }
+  componentWillMount(){
+    bonds.allAccountsInfo.tie(this.renderAccounts.bind(this));
+  }
   render(){
-    return (<AddressCard />);
+    //bonds.allAccountsInfo.map(b => console.log('b', b))
+    console.log('accs',this.state.accounts);
+    return (<div>
+      {this.state.accounts.map((addr)=>{
+        return (<AddressCard
+          address={addr}          
+        />);
+      })}
+      </div>);
+  }
+
+  renderAccounts(accountList){
+
+    if(typeof accountList != 'undefined'){
+      console.log('hereo', accountList);
+      let p = Object.keys(accountList)
+      .filter((key) => {
+        return (typeof accountList[key].uuid == 'undefined') && (!accountList[key].meta.contract) && !accountList[key].meta.wallet;
+      })
+      .map((elem)=>{
+        return elem;
+      })
+      console.log('p', p);
+      this.setState({accounts:p});
+    }
   }
 }
