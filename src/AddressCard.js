a5125fd8c0108a84d910e8a9f94d08eaf6cfb767
Arthur Stemmer <apstemmer@hotmail.com>
2017-09-28 11:55:27 +0100

    imgs show, needs more styling

--- src/AddressCard.js
+++ src/AddressCard.js
@@ -17,11 +17,14 @@ export default class AddressCard extends Component{
     let addressBond = new Bond();
     addressBond.changed(this.props.info.address);
     let balanceBond = bonds.balance(this.props.info.address);
-    let bondi = (new Bond).changed('0xd40679a3a234d8421c678d64f4df3308859e8ad07ac95ce4a228aceb96955287');
-    bonds.tokens.then((ls)=>{
-      bonds.githubhint.entries(ls[0].img).then(arr=>{console.log(arr[0])})
-    });
-    console.log('accinfo',);
+    let bondi = bonds.tokensOf(this.props.info.address).then((ret)=>{
+      //console.log('tokens',ret)
+    })
+    bonds.githubhint.entries("0xd40679a3a234d8421c678d64f4df3308859e8ad07ac95ce4a228aceb96955287").then((ret)=>{
+      //console.log('imgs',ret)
+    })
+
+    //console.log('accinfo',);
     return (<Card className={styles.AddressCard} >
         <Card.Content>
           <Image floated="right"><AccountIcon address={this.props.info.address} /></Image>
@@ -31,17 +34,38 @@ export default class AddressCard extends Component{
       <Card.Content>
         <AddressLabel address={addressBond} />
       </Card.Content>
-      <Rspan>{}</Rspan>
-      <CoinList />
+      <Rspan></Rspan>
+      <CoinList tokens={bonds.tokensOf(this.props.info.address)}/>
     </Card>);
   }
 }
 
 class CoinList extends ReactiveComponent{
   constructor(){
-    super();
+    super(['tokens']);
   }
   render(){
-    return <p></p>;
+    //console.log('toki',this.state.tokens);
+    if(typeof this.state.tokens == "undefined") return <Card.Content></Card.Content>
+    return (<Card.Content>
+      {this.state.tokens.map((elem)=>{
+        return (<div>
+          <span>{elem.name}:{elem.balance.toString()}</span>
+          <Rimg src={bonds.githubhint.entries(elem.img).map((imgObj)=>{
+            console.log('img', imgObj[0]);
+            return imgObj[0]
+          })}></Rimg>
+        </div>);
+      })}
+    </Card.Content>);
   }
 }
+//
+// let ownedTokenBond = bonds.tokens.map((ls)=>{
+//   console.log(ls[0].tla);
+//   let owned = []
+//   for(let num in ls){
+//     if(ls[num].tla == "GAV") owned.push(ls[num]);//need new way to filtr
+//   }
+//   return owned;
+// })
